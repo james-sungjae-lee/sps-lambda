cfbace7f7232dbdac4867cb5e1ff5765697595cf
James Sungjae Lee <odobenuseKR@gmail.com>
2021-11-04 17:19:38 +0900

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -3,12 +3,13 @@ import json
 import boto3
 
 ec2 = boto3.client('ec2')
+print(boto3.__version__)
 
 def handler(event, context):
     instance_types = ast.literal_eval(event['instance_types'])
     regions = ast.literal_eval(event['regions'])
     
-    reponse = ec2.get_spot_placement_scores(InstanceTypes=instance_types,
+    response = ec2.get_spot_placement_scores(InstanceTypes=instance_types,
     TargetCapacity=1, SingleAvailabilityZone=True, RegionNames=regions)
     scores = response['SpotPlacementScores']
     
