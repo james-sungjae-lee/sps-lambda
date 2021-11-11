1e98348cc147b67c44e160c7c16062636f770502
James Sungjae Lee <odobenuseKR@gmail.com>
2021-11-11 10:42:42 +0900

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -2,14 +2,21 @@ import ast
 import json
 import boto3
 
-ec2 = boto3.client('ec2')
-
 def lambda_handler(event, context):
+    credential = event['credential']
     instance_types = ast.literal_eval(event['instance_types'])
     regions = ast.literal_eval(event['regions'])
     
-    response = ec2.get_spot_placement_scores(InstanceTypes=instance_types,
-    TargetCapacity=1, SingleAvailabilityZone=True, RegionNames=regions)
+    session = boto3.session.Session(
+        aws_access_key_id=credential['aws_id'],
+        aws_secret_access_key=credential['aws_pw'])
+    ec2 = session.client('ec2', region_name='us-west-2')
+    
+    response = ec2.get_spot_placement_scores(
+        InstanceTypes=instance_types,
+        TargetCapacity=1,
+        SingleAvailabilityZone=True,
+        RegionNames=regions)
     scores = response['SpotPlacementScores']
     
     return {
